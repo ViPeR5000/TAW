a2f01f368bbf5b1e4c930c0a6c4efcadf64988c6
ViPeR5000(Rui Melo) <viper5000@viper5000.net>
2025-12-13 02:05:56 +0000

    limpeza da pasta p2

--- projeto_taw_grupo_2/server.js
+++ projeto_taw_grupo_2/server.js
@@ -1,15 +1,21 @@
 const express = require('express'); // Importa o Express (padrão da indústria em Node.js para criar servidores web, rotas API (REST) e middleware)
 const mongoose = require('mongoose'); // Importa o Mongoose para interação com a base de dados
- 
+
 const app = express(); // Cria uma instância da aplicação. Será utilizado para definir as rotas, configurações e middleware do servidor
 const PORT = process.env.PORT || 3001; // Define o número da porta de rede onde o servidor web irá estar à escuta de pedidos
- 
+
 // Middlewares
 app.use(express.json()); // Configura o Express para processar pedidos que chegam ao servidor com o header Content-Type: application/json.
- 
+
+// Rota de teste simples
+app.get('/', (req, res) => { // Ficará à escuta de pedidos HTTP que usam o método GET no caminho / (ou seja, a raiz ou root da aplicação).
+    // Define a função callback que é executada sempre que um pedido corresponde à rota /. 
+    res.send('A API de Utilizadores está ON!'); // Envia a resposta ao cliente
+});
+
 // Define a string de conexão a partir da variável de ambiente (MONGO_URI)
 const DB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/projeto-db';
- 
+
 mongoose.connect(DB_URI) // Inicia a tentativa de conexão assíncrona à base de dados MongoDB
     .then(() => { // Esta função é executada apenas se a ligação à base de dados for bem-sucedida
         console.log('Ligação bem-sucedida ao MongoDB!');
@@ -20,4 +26,3 @@ mongoose.connect(DB_URI) // Inicia a tentativa de conexão assíncrona à base d
     .catch(err => { // Esta função é executada apenas se a ligação ao MongoDB falhar
         console.error('ERRO: Falha na ligação ao MongoDB:', err.message);
     });
-    
