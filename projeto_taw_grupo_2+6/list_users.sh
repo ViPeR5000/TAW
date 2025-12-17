bf30bb442c24778d815108421cb238a47997c322
ViPeR5000(Rui Melo) <viper5000@viper5000.net>
2025-12-17 04:01:06 +0000

    Everything will be just tickety-boo today.

---
+++ projeto_taw_grupo_2+6/list_users.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Nome do container definido no docker-compose.yml
+CONTAINER_NAME="mongodb-db-etapa2-6"
+DB_NAME="projeto-db"
+
+echo "A listar todos os utilizadores da base de dados '$DB_NAME'..."
+
+
+docker exec $CONTAINER_NAME mongosh $DB_NAME --eval "db.users.find()" || \
+docker exec $CONTAINER_NAME mongo $DB_NAME --eval "db.users.find()"
+
+echo "--- Fim da Lista ---"
